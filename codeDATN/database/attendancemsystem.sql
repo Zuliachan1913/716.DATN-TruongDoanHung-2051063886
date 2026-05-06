8d8be16915618bd4324320e1ab986201e44e9727
Trương Doãn Hùng <138448757+Zuliachan1913@users.noreply.github.com>
2026-05-06 17:29:47 +0700

    Update attendancemsystem.sql

--- codeDATN/database/attendancemsystem.sql
+++ codeDATN/database/attendancemsystem.sql
@@ -1,1 +1,363 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th5 06, 2026 lúc 12:29 PM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.2.12
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `Id` int(10) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `emailAddress` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`Id`, `firstName`, `lastName`, `emailAddress`, `password`) VALUES
+(1, 'Admin', '', 'admin@mail.com', 'D00F5D5217896FB7FD601412CB890830');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tblattendance`
+--
+
+CREATE TABLE `tblattendance` (
+  `Id` int(10) NOT NULL,
+  `admissionNo` varchar(255) NOT NULL,
+  `classId` varchar(10) NOT NULL,
+  `classArmId` varchar(10) NOT NULL,
+  `sessionTermId` varchar(10) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `dateTimeTaken` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `tblattendance`
+--
+
+INSERT INTO `tblattendance` (`Id`, `admissionNo`, `classId`, `classArmId`, `sessionTermId`, `status`, `dateTimeTaken`) VALUES
+(162, 'ASDFLKJ', '1', '2', '1', '1', '2026-05-03'),
+(163, 'HSKSDD', '1', '2', '1', '1', '2026-05-03'),
+(164, 'JSLDKJ', '1', '2', '1', '1', '2026-05-03'),
+(172, 'HSKDS9EE', '1', '4', '1', '1', '2026-05-03'),
+(171, 'JKADA', '1', '4', '1', '0', '2026-05-03'),
+(170, 'JSFSKDJ', '1', '4', '1', '1', '2026-05-03'),
+(173, 'ASDFLKJ', '1', '2', '1', '1', '2026-05-03'),
+(174, 'HSKSDD', '1', '2', '1', '1', '2026-05-03'),
+(175, 'JSLDKJ', '1', '2', '1', '1', '2026-05-03'),
+(176, 'JSFSKDJ', '1', '4', '1', '0', '2026-05-03'),
+(177, 'JKADA', '1', '4', '1', '0', '2026-05-03'),
+(178, 'HSKDS9EE', '1', '4', '1', '0', '2026-05-03'),
+(179, 'ASDFLKJ', '1', '2', '1', '0', '2026-05-03'),
+(180, 'HSKSDD', '1', '2', '1', '1', '2026-05-03'),
+(181, 'JSLDKJ', '1', '2', '1', '1', '2026-05-03'),
+(182, 'ASDFLKJ', '1', '2', '1', '0', '2026-05-03'),
+(183, 'HSKSDD', '1', '2', '1', '0', '2026-05-03'),
+(184, 'JSLDKJ', '1', '2', '1', '1', '2026-05-03'),
+(185, 'ASDFLKJ', '1', '2', '1', '0', '2026-05-03'),
+(186, 'HSKSDD', '1', '2', '1', '0', '2026-05-03'),
+(187, 'JSLDKJ', '1', '2', '1', '0', '2026-05-03'),
+(188, 'AMS110', '4', '6', '1', '1', '2026-05-03'),
+(189, 'AMS133', '4', '6', '1', '0', '2026-05-03'),
+(190, 'AMS135', '4', '6', '1', '0', '2026-05-03'),
+(191, 'AMS144', '4', '6', '1', '1', '2026-05-03'),
+(192, 'AMS148', '4', '6', '1', '0', '2021-10-07'),
+(193, 'AMS151', '4', '6', '1', '1', '2021-10-07'),
+(194, 'AMS159', '4', '6', '1', '1', '2021-10-07'),
+(195, 'AMS161', '4', '6', '1', '1', '2021-10-07'),
+(196, 'AMS110', '4', '6', '1', '1', '2022-06-06'),
+(197, 'AMS133', '4', '6', '1', '0', '2022-06-06'),
+(198, 'AMS135', '4', '6', '1', '0', '2022-06-06'),
+(199, 'AMS144', '4', '6', '1', '1', '2022-06-06'),
+(200, 'AMS148', '4', '6', '1', '0', '2022-06-06'),
+(201, 'AMS151', '4', '6', '1', '1', '2022-06-06'),
+(202, 'AMS159', '4', '6', '1', '1', '2022-06-06'),
+(203, 'AMS161', '4', '6', '1', '1', '2022-06-06');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tblclass`
+--
+
+CREATE TABLE `tblclass` (
+  `Id` int(10) NOT NULL,
+  `className` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `tblclass`
+--
+
+INSERT INTO `tblclass` (`Id`, `className`) VALUES
+(1, 'Seven'),
+(3, 'Eight'),
+(4, 'Nine');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tblclassarms`
+--
+
+CREATE TABLE `tblclassarms` (
+  `Id` int(10) NOT NULL,
+  `classId` varchar(10) NOT NULL,
+  `classArmName` varchar(255) NOT NULL,
+  `isAssigned` varchar(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `tblclassarms`
+--
+
+INSERT INTO `tblclassarms` (`Id`, `classId`, `classArmName`, `isAssigned`) VALUES
+(2, '1', 'S1', '1'),
+(4, '1', 'S2', '1'),
+(5, '3', 'E1', '1'),
+(6, '4', 'N1', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tblclassteacher`
+--
+
+CREATE TABLE `tblclassteacher` (
+  `Id` int(10) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `emailAddress` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phoneNo` varchar(50) NOT NULL,
+  `classId` varchar(10) NOT NULL,
+  `classArmId` varchar(10) NOT NULL,
+  `dateCreated` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `tblclassteacher`
+--
+
+INSERT INTO `tblclassteacher` (`Id`, `firstName`, `lastName`, `emailAddress`, `password`, `phoneNo`, `classId`, `classArmId`, `dateCreated`) VALUES
+(4, 'Demola', 'Ade', 'teacher3@gmail.com', '32250170a0dca92d53ec9624f336ca24', '09672002882', '1', '4', '2026-05-01'),
+(5, 'Ryan', 'Mbeche', 'teacher4@mail.com', '32250170a0dca92d53ec9624f336ca24', '7014560000', '3', '5', '2026-05-06'),
+(6, 'John', 'Keroche', 'teacher@mail.com', '32250170a0dca92d53ec9624f336ca24', '0100000030', '4', '6', '2026-05-06');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tblsessionterm`
+--
+
+CREATE TABLE `tblsessionterm` (
+  `Id` int(10) NOT NULL,
+  `sessionName` varchar(50) NOT NULL,
+  `termId` varchar(50) NOT NULL,
+  `isActive` varchar(10) NOT NULL,
+  `dateCreated` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `tblsessionterm`
+--
+
+INSERT INTO `tblsessionterm` (`Id`, `sessionName`, `termId`, `isActive`, `dateCreated`) VALUES
+(1, '2021/2022', '1', '1', '2026-05-03'),
+(3, '2021/2022', '2', '0', '2026-05-03');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tblstudents`
+--
+
+CREATE TABLE `tblstudents` (
+  `Id` int(10) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `otherName` varchar(255) NOT NULL,
+  `admissionNumber` varchar(255) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `classId` varchar(10) NOT NULL,
+  `classArmId` varchar(10) NOT NULL,
+  `dateCreated` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `tblstudents`
+--
+
+INSERT INTO `tblstudents` (`Id`, `firstName`, `lastName`, `otherName`, `admissionNumber`, `password`, `classId`, `classArmId`, `dateCreated`) VALUES
+(1, 'Thomas', 'Omari', 'none', 'AMS005', '12345', '1', '2', '2026-05-03'),
+(3, 'Samuel', 'Ondieki', 'none', 'AMS007', '12345', '1', '2', '2026-05-03'),
+(4, 'Milagros', 'Oloo', 'none', 'AMS011', '12345', '1', '2', '2026-05-03'),
+(5, 'Luis', 'Ayo', 'none', 'AMS012', '12345', '1', '4', '2026-05-03'),
+(6, 'Sandra', 'Sagero', 'none', 'AMS015', '12345', '1', '4', '2026-05-03'),
+(7, 'Smith', 'Makori', 'Mack', 'AMS017', '12345', '1', '4', '2026-05-03'),
+(8, 'Juliana', 'Kerubo', 'none', 'AMS019', '12345', '3', '5', '2026-05-03'),
+(9, 'Richard', 'Semo', 'none', 'AMS021', '12345', '3', '5', '2026-05-03'),
+(10, 'Jon', 'Mbeeka', 'none', 'AMS110', '12345', '4', '6', '2026-05-03'),
+(11, 'Aida', 'Moraa', 'none', 'AMS133', '12345', '4', '6', '2026-05-03'),
+(12, 'Miguel', 'Bush', 'none', 'AMS135', '12345', '4', '6', '2026-05-03'),
+(13, 'Sergio', 'Hammons', 'none', 'AMS144', '12345', '4', '6', '2026-05-03'),
+(14, 'Lyn', 'Rogers', 'none', 'AMS148', '12345', '4', '6', '2026-05-03'),
+(15, 'James', 'Dominick', 'none', 'AMS151', '12345', '4', '6', '2026-05-03'),
+(16, 'Ethel', 'Quin', 'none', 'AMS159', '12345', '4', '6', '2026-05-03'),
+(17, 'Roland', 'Estrada', 'none', 'AMS161', '12345', '4', '6', '2026-05-03');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `tblterm`
+--
+
+CREATE TABLE `tblterm` (
+  `Id` int(10) NOT NULL,
+  `termName` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `tblterm`
+--
+
+INSERT INTO `tblterm` (`Id`, `termName`) VALUES
+(1, 'First'),
+(2, 'Second'),
+(3, 'Third');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Chỉ mục cho bảng `tblattendance`
+--
+ALTER TABLE `tblattendance`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Chỉ mục cho bảng `tblclass`
+--
+ALTER TABLE `tblclass`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Chỉ mục cho bảng `tblclassarms`
+--
+ALTER TABLE `tblclassarms`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Chỉ mục cho bảng `tblclassteacher`
+--
+ALTER TABLE `tblclassteacher`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Chỉ mục cho bảng `tblsessionterm`
+--
+ALTER TABLE `tblsessionterm`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Chỉ mục cho bảng `tblstudents`
+--
+ALTER TABLE `tblstudents`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Chỉ mục cho bảng `tblterm`
+--
+ALTER TABLE `tblterm`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT cho bảng `tblattendance`
+--
+ALTER TABLE `tblattendance`
+  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=204;
+
+--
+-- AUTO_INCREMENT cho bảng `tblclass`
+--
+ALTER TABLE `tblclass`
+  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT cho bảng `tblclassarms`
+--
+ALTER TABLE `tblclassarms`
+  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT cho bảng `tblclassteacher`
+--
+ALTER TABLE `tblclassteacher`
+  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT cho bảng `tblsessionterm`
+--
+ALTER TABLE `tblsessionterm`
+  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT cho bảng `tblstudents`
+--
+ALTER TABLE `tblstudents`
+  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT cho bảng `tblterm`
+--
+ALTER TABLE `tblterm`
+  MODIFY `Id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
